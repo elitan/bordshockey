76aee84f319cf7a0f9405e7a5c7e38d4f8ea561e
elitan <johan@eliasson.me>
2019-08-09 08:02:10 +0200

    update

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Subscription } from 'react-apollo';
 import styled from 'styled-components';
+import classNames from 'classnames';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -18,10 +19,27 @@ S.DashBoard = styled.div`
   .paper {
     margin: 3rem 0 3rem 0;
   }
+
+  .highlight {
+    background: #e8f7ff;
+
+    .match_win {
+      font-weight: bold;
+    }
+  }
 }
 `;
 
 class DashBoard extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected_team_id: null,
+    };
+  }
+
   render() {
     return (
       <Layout>
@@ -34,8 +52,6 @@ class DashBoard extends Component {
               if (loading) return 'Loading...';
               if (error) return `Error! ${error.message}`;
 
-              console.log(data);
-
               const { tablez } = data;
 
               return (
@@ -45,24 +61,38 @@ class DashBoard extends Component {
                       <TableRow>
                         <TableCell>#</TableCell>
                         <TableCell>Name</TableCell>
-                        <TableCell>Points</TableCell>
                         <TableCell>Matches</TableCell>
+                        <TableCell>Points</TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
-                      {tablez.map((row, i) => (
-                        <TableRow key={i} hover>
+                      {tablez.map((team, i) => (
+                        <TableRow
+                          key={team.id}
+                          hover
+                          className="tablez-team"
+                          onMouseOver={() => {
+                            this.setState({
+                              team_id: team.id,
+                            });
+                          }}
+                          onMouseOut={() => {
+                            this.setState({
+                              team_id: null,
+                            });
+                          }}
+                        >
                           <TableCell>
                             {i + 1}
                           </TableCell>
                           <TableCell>
-                            {row.name}
+                            {team.name}
                           </TableCell>
                           <TableCell>
-                            {row.points}
+                            {team.matches_played}
                           </TableCell>
                           <TableCell>
-                            {row.matches_played}
+                            {team.points}
                           </TableCell>
                         </TableRow>
                       ))}
@@ -80,8 +110,8 @@ class DashBoard extends Component {
               if (loading) return 'Loading...';
               if (error) return `Error! ${error.message}`;
 
-              console.log(data);
               const { matches } = data;
+
               return (
                 <Paper className="paper paper-matches">
                   <Table>
@@ -93,19 +123,56 @@ class DashBoard extends Component {
                       </TableRow>
                     </TableHead>
                     <TableBody>
-                      {matches.map((match, i) => (
-                        <TableRow key={i} hover>
-                          <TableCell>
-                            {match.home_team_name}
-                          </TableCell>
-                          <TableCell>
-                            {match.away_team_name}
-                          </TableCell>
-                          <TableCell>
-                            {match.home_team_score} - {match.away_team_score}
-                          </TableCell>
-                        </TableRow>
-                      ))}
+                      {matches.map((match, i) => {
+
+                        let highlight = false;
+                        let home_team_win = false;
+                        let away_team_win = false;
+
+                        if (match.home_team_id === this.state.team_id || match.away_team_id === this.state.team_id) {
+                          highlight = true;
+                          if (match.home_team_score > match.away_team_score) {
+                            home_team_win = true;
+                          } else {
+                            away_team_win = true;
+                          }
+                        }
+
+                        const row_classes = classNames({
+                          highlight,
+                        });
+
+                        const home_classes = classNames({
+                          match_win: home_team_win,
+                        });
+
+                        const away_classes = classNames({
+                          match_win: away_team_win,
+                        });
+
+
+                        return (
+                          <TableRow
+                            key={i}
+                            hover
+                            className={row_classes}
+                          >
+                            <TableCell
+                              className={home_classes}
+                            >
+                              {match.home_team_name}
+                            </TableCell>
+                            <TableCell
+                              className={away_classes}
+                            >
+                              {match.away_team_name}
+                            </TableCell>
+                            <TableCell>
+                              {match.home_team_score} - {match.away_team_score}
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
                     </TableBody>
                   </Table>
                 </Paper>
--- src/Components/gql/Hasura.js
+++ src/Components/gql/Hasura.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 export const S_GET_TABLEZ = gql`
 subscription {
   tablez (order_by: { points: desc}) {
+    id
     name
     points
     matches_played
@@ -13,9 +14,11 @@ subscription {
 export const S_GET_MATCHES = gql`
 subscription {
   matches: matches_with_points (order_by: {created_at: desc}) {
+    home_team_id
     home_team_name
     home_team_score
     home_team_points
+    away_team_id
     away_team_name
     away_team_score
     away_team_points
